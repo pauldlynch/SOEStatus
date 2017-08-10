15218248d6445fb3a5a327637166ba64feb014c6
Paul Lynch <paul@plsys.co.uk>
2017-08-10 11:06:27 +0100

    Add Imgur image search, convert to NSURLSession, and refactor.

--- SOEStatus/PhotoSearch.m
+++ SOEStatus/PhotoSearch.m
@@ -12,26 +12,57 @@
 
 @implementation PhotoSearch
 
++ (void)fetchJSONFromRequest:(NSURLRequest *)request completion:(void (^)(NSDictionary *results))completion {
+    NSURLSessionDataTask *downloadTask = [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (!data) {
+            NSLog(@"%s failed call to API: %@", __PRETTY_FUNCTION__, error);
+            if (completion) completion(nil);
+            return;
+        }
+        NSDictionary *results = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        if (!results) {
+            NSLog(@"%s bad JSON from API: %@ '%@'", __PRETTY_FUNCTION__, error, [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+        }
+        
+        if (completion) completion(results);
+    }];
+    [downloadTask resume];
+}
+
++ (void)fetchJSONFromURL:(NSURL *)url completion:(void (^)(NSDictionary *results))completion {
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    NSString *authHeaderValue = [NSString stringWithFormat:@"Client-ID %@", ImgurAPIKey];
+    [request addValue:authHeaderValue forHTTPHeaderField:@"authorization"];
+    
+    [PhotoSearch fetchJSONFromRequest:request completion:completion];
+}
+
 + (void)callFlickr:(NSString *)urlString completion:(void (^)(NSDictionary *results))completion {
     NSURL *url = [NSURL URLWithString:urlString];
-    NSData *jsonData = [NSData dataWithContentsOfURL:url];
-    if (!jsonData) {
-        NSLog(@"%s failed call to Flickr API", __PRETTY_FUNCTION__);
-        return;
-    }
-    NSError *error;
-    NSDictionary *results = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&error];
-    if (!results) {
-        NSLog(@"%s bad JSON from Flickr API: %@ '%@'", __PRETTY_FUNCTION__, error, [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding]);
-        return;
-    }
+    [PhotoSearch fetchJSONFromURL:url completion:^(NSDictionary *results){
+        NSString *status = [results objectForKey:@"stat"];
+        if (![status isEqualToString:@"ok"]) {
+            NSLog(@"Flickr API not good: %@ code %@ '%@'", status, [results objectForKey:@"code"], [results objectForKey:@"message"]);
+        }
+        
+        if (completion) completion(results);
+    }];
+}
+
++ (void)callImgur:(NSString *)urlString completion:(void (^)(NSDictionary *results))completion {
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    NSString *authHeaderValue = [NSString stringWithFormat:@"Client-ID %@", ImgurAPIKey];
+    [request addValue:authHeaderValue forHTTPHeaderField:@"authorization"];
     
-    NSString *status = [results objectForKey:@"stat"];
-    if (![status isEqualToString:@"ok"]) {
-        NSLog(@"Flickr API not good: %@ code %@ '%@'", status, [results objectForKey:@"code"], [results objectForKey:@"message"]);
-    }
-    
-    if (completion) completion(results);
+    [PhotoSearch fetchJSONFromRequest:request completion:^(NSDictionary *results){
+        NSNumber *success = [results objectForKey:@"success"];
+        if (![success boolValue]) {
+            NSLog(@"Imgur API not good: %@ status %@", success, [results objectForKey:@"status"]);
+        }
+        
+        if (completion) completion(results);
+    }];
 }
 
 - (void)photoSearch:(NSString *)searchString completion:(void (^)(void))completion {
@@ -41,6 +72,7 @@
     
     NSLog(@"PhotoSearch requesting: %@", searchString);
     
+    // start with queueing Flickr request
     NSString *urlString = [NSString stringWithFormat:@"https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=%@&tags=%@&per_page=20&format=json&nojsoncallback=1&content_type=7&safe_search=2", FlickrAPIKey, [searchString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
     [PhotoSearch callFlickr:urlString completion:^(NSDictionary *results){
         NSArray *photos = [results valueForKeyPath:@"photos.photo"];
@@ -56,6 +88,29 @@
                 dispatch_group_leave(photo_group);
             }];
         }
+    }];
+    
+    // Now queue Imgur request
+    dispatch_group_enter(photo_group);
+    urlString = [NSString stringWithFormat:@"https://api.imgur.com/3/gallery/search/?q=%@", [searchString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    [PhotoSearch callImgur:urlString completion:^(NSDictionary *results){
+        NSArray *photos = [results valueForKeyPath:@"data"];
+        if (![photos isKindOfClass:[NSArray class]]) photos = nil; // returns dict on error
+        NSLog(@"imgur returned %lu for %@", (unsigned long)[photos count], searchString);
+        for (NSDictionary *photo in photos) {
+            NSString *photoURLString = [photo objectForKey:@"link"];
+            NSNumber *isAlbum = [photo objectForKey:@"is_album"];
+            if (![isAlbum boolValue]) { // skip albums for now
+                if ([photoURLString hasPrefix:@"http:"]) {
+                    photoURLString = [photoURLString stringByReplacingOccurrencesOfString:@"http:" withString:@"https:"];
+                }
+                NSURL *url = [NSURL URLWithString:photoURLString];
+                [self.photoURLs addObject:url];
+                NSString *title = [photo objectForKey:@"title"];
+                [self.photoNames addObject:([title length] > 0 ? title : @"Untitled")];
+            }
+        }
+        dispatch_group_leave(photo_group);
     }];
     
     dispatch_group_notify(photo_group, dispatch_get_main_queue(), ^{
